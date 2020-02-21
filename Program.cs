02530a9f4ef9b24664887951ea989c242a8718ad
BatOleh <61079990+BatOleh@users.noreply.github.com>
2020-02-21 23:26:03 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2_homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            double p, S, Sn;
+            Console.Write("Enter years: ");
+            y = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter persents: ");
+            p = Int32.Parse(Console.ReadLine());
+            Console.Write("Enter Sum: ");
+            S = Int32.Parse(Console.ReadLine());
+            Sn = (S + (S * (p / 100.0)*y));
+            Console.WriteLine("Sum after "+y+" years = "+Sn);
+            Console.ReadKey();
+        }
+    }
+}
